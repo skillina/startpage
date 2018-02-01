2070da9fe71a672a58c4096930952289d8dffc97
Alexander Skillin <skillinalex@gmail.com>
2018-01-31 23:32:05 -0500

    Old progress. Not much but a starting point

---
+++ start.js
@@ -0,0 +1,16 @@
+function greeting(){
+    var d = new Date();
+    hour = d.getHours();
+
+    var str = "Hello, Alexander";
+    
+    if(hour < 12){
+	str = "Good Morning, Alexander.";
+    }else if(hour < 19){
+	str = "Good Afternoon, Alexander.";
+    }else{
+	str = "Good Evening, Alexander.";
+    }
+
+    return str;
+}
