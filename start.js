3525c8efbba2e364d388b4c484032d885dfdf6c3
Alexander Skillin <skillinalex@gmail.com>
2018-02-01 00:14:31 -0500

    Added some necessary snark to the welcome message

--- start.js
+++ start.js
@@ -4,12 +4,14 @@ function greeting(){
 
     var str = "Hello, Alexander";
     
-    if(hour < 12){
-	str = "Good Morning, Alexander.";
+    if(hour < 4){
+        str = "Another late night, Alex?";
+    }else if(hour < 12){
+    	str = "Good Morning, Alexander.";
     }else if(hour < 19){
-	str = "Good Afternoon, Alexander.";
+    	str = "Good Afternoon, Alexander.";
     }else{
-	str = "Good Evening, Alexander.";
+    	str = "Good Evening, Alexander.";
     }
 
     return str;
